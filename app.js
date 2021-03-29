de6160e0c7fd11ffba857ea9f320fd51349b2f56
Bigjames728 <bigjames728@gmail.com>
2021-03-29 11:37:13 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+// app.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "3000";
+
+/**
+ *  App Configuration
+ */
+
+app.set("view engine", "pug");
+app.use(express.static('public'));
+
+/**
+ * Routes Definitions
+ */
+
+app.get("/", (req, res) => {
+    res.render('home');
+});
+
+/**
+ * Server Activation
+ */
+
+ app.listen(port, () => {
+    console.log(`The app is running on localhost:${port}`);
+});
