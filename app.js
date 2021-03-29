215aee727babed7ec75e9e1d15590cee8d3dc95e
Bigjames728 <bigjames728@gmail.com>
2021-03-29 12:08:37 -0700

    required data.json

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 
 const express = require("express");
 const path = require("path");
+const data = require("data.json");
 
 /**
  * App Variables
@@ -27,6 +28,7 @@ app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
+    req.app.locals = data.projects;
 });
 
 /**
