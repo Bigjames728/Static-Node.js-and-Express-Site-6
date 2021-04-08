1cadc0d5978ad05ac203e04ad2be01f0aaed798a
Bigjames728 <bigjames728@gmail.com>
2021-04-08 12:43:38 -0700

    Trying to fix error handlers.

--- app.js
+++ app.js
@@ -47,24 +47,38 @@ app.use('/', projectRouter);
 
 /* 404 handler to catch undefined or non-existent route requests */
 app.use((req, res, next) => {
+    console.log('404 error handler called');
 
-    const err = new Error();
-    err.message = `Oops! It looks like this page doesn't exist.`;
-    err.status = 404;
-    console.log(err.message);
-    res.render('page-not-found', { err });
+    /* TODO 1: Send a response to the client
+        - Set the response status to 404
+        - Render the 'page-not-found'view
+    */
+   res.status(404).render('page-not-found');
+    
 });
 
-// /* Global error handler */
+/* Global error handler */
 
 app.use((err, req, res, next) => {
+
+    if (err) {
+        console.log('Global error handler called', err);
+    }
+
+    /* TODO 2: Handle errors caught by your route handlers
+        - If the error status is 404:
+            * Set the response status to 404
+            * Render the 'page-not-found' view and pass the error object to the view
+        - Else:
+            * Set the error message to the given message, or specify a general, defalut error message
+            * Set response status to the given error status OR, set it to 500 by default if no error status is set
+            * Render the 'error' view, passing it the error object
+    */
     if (err.status === 404) {
         res.status(404).render('page-not-found', { err });
     } else {
-        err.message = 'Oops! It looks like something went wrong on the server.';
-        err.status = 500;
-        res.render('error', { err });
-        console.log(err.message);
+        err.message = err.message || 'Oops! It looks like something went wrong with the server.';
+        res.status(err.status || 500).render('error', { err });
     }
 });
 
@@ -75,3 +89,5 @@ app.use((err, req, res, next) => {
  app.listen(port, () => {
     console.log(`The app is running on localhost:${port}`);
 });
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const data = require("./data.json");
+const data = require("../data");
 const projects = data.data.projects;
 
 
@@ -16,6 +16,19 @@ router.get("/about", (req, res) => {
     res.render("about");
 });
 
+/*GET generated error route - create and throw 500 server error **/
+
+router.get('/error', (req, res, next) => {
+
+    // Log out custom error handler indication
+    console.log('custom error route called');
+
+    const err = new Error();
+    err.message = 'Custom 500 error thrown'
+    err.status = 500;
+    throw err;
+});
+
 // Project route that renders a customized version of the project.pug template based on which ID was selected
 
 router.get("/projects/:id", (req, res, next) => {
@@ -23,8 +36,10 @@ router.get("/projects/:id", (req, res, next) => {
     if (project) {
         res.render('project', { project });
     } else {
-        console.log("The page your looking for doesn't exist");
-        res.status(404).render('page-not-found');
+        const err = new Error();
+        err.status = 404;
+        err.message = "Looks like the project you requested doesn't exist.";
+        next(err);
     }
 });
 
